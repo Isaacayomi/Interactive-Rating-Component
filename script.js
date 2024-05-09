b47731b2b28f3173020e496abc52ce19fb822ef7
Isaac Ayomide <okunlolaa3@outlook.com>
2024-05-09 21:55:19 +0100

    little fix

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const container = document.querySelector('.container')
-const buttonsEl = document.querySelectorAll('.btn');
-const modal = document.querySelector('.modal')
-const rate = document.querySelector('.rate')
-const submit = document.querySelector('.submit')
-const rate_again = document.querySelector('.rate_again')
+const bodyEl = document.querySelector(".el");
+const container = document.querySelector(".container");
+const buttonsEl = document.querySelectorAll(".btn");
+const modal = document.querySelector(".modal");
+const rate = document.querySelector(".rate");
+const submit = document.querySelector(".submit");
 
-for (let i = 0; i < buttonsEl.length; i++) {
-    buttonsEl[i].addEventListener('click', function () {
-        if (!buttonsEl[i].classList.contains('clicked')) {
-            buttonsEl[i].classList.add('clicked')
-        } else {
-            buttonsEl[i].classList.remove('clicked')
-        }
+const selectRateBtn = function () {
+  for (let i = 0; i < buttonsEl.length; i++) {
+    buttonsEl[i].addEventListener("click", () => {
+      if (!buttonsEl[i].classList.contains("clicked")) {
+        buttonsEl[i].classList.add("clicked");
+      } else {
+        buttonsEl[i].classList.remove("clicked");
+      }
 
-        //Stores the content of the button i clicked into rate innerText
-        rate.textContent = buttonsEl[i].textContent
-    })
-}
+      //Stores the content of the button i clicked into rate innerText
+      rate.textContent = buttonsEl[i].textContent;
+    });
+  }
+};
+selectRateBtn();
 
-submit.addEventListener('click', function () {
-    container.classList.add('hidden')
-    modal.classList.remove('hidden')
-})
+const submitRating = function () {
+  container.classList.add("hidden");
+  modal.classList.remove("hidden");
+};
+
+submit.addEventListener("click", submitRating);
